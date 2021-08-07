95d29e5e7e2bd9eabaecdae46631738b4412a31a
superuserz <manmeet.devgun@yahoo.in>
2021-08-08 00:54:58 +0530

    refactor

--- src/test/java/com/nagp/devops/jenkins/NagpDevopsApplicationTests.java
+++ src/test/java/com/nagp/devops/jenkins/NagpDevopsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.nagp.devops.jenkins;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.nagp.devops.jenkins.controller.*;
 
 @SpringBootTest
 class NagpDevopsApplicationTests {
 
+private static final Logger LOGGER = LoggerFactory.getLogger(NagpDevopsApplicationTests.class);
+	
+	@Autowired
+	private DemoController controller;
+	
 	@Test
-	void contextLoads() {
+	void testFetchCatalog() throws Exception {
+		String value = controller.getServerStatus();
+		LOGGER.info("Server Status : " + value);
+		assertThat(value).contains("Healthy");
+		
+			
 	}
+	
+	
+	@Test
+	void testFetchCatalog2() throws Exception {
+		String value = controller.getServerMessage();
+		LOGGER.info("Server said : " + value);
+		assertThat(value).contains("Hello");
+		
+	}
+	
+
 
 }
